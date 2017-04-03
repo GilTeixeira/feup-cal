361c735b2aef9712e83e98f5ae48820b9ad8a81c
r_tor <r_torres96@live.com.pt>
2017-04-03 16:49:17 +0100

    added graphviwer getting some weird error need to ask teacher :)

--- agencia de viagens/agencia de viagens/Main.cpp
+++ agencia de viagens/agencia de viagens/Main.cpp
@@ -5,16 +5,16 @@
 #include <iomanip>
 #include <string>
 
-int main() {
-	try {
-		TravelAgency travelAgency= TravelAgency();
-		mainMenu(travelAgency);
-	}
-	catch(exception_or_error x) {
-		cerr << x.get_reason() << ". Tente novamente.\n";
-		system("PAUSE");
-		clrscr();
-		return 1;
-	}
-	return 0;
-}
+//int main() {
+//	try {
+//		TravelAgency travelAgency= TravelAgency();
+//		mainMenu(travelAgency);
+//	}
+//	catch(exception_or_error x) {
+//		cerr << x.get_reason() << ". Tente novamente.\n";
+//		system("PAUSE");
+//		clrscr();
+//		return 1;
+//	}
+//	return 0;
+//}
--- agencia de viagens/agencia de viagens/TravelAgency.h
+++ agencia de viagens/agencia de viagens/TravelAgency.h
@@ -2,6 +2,7 @@
 #include "utils.h"
 #include "exceptions.h"
 #include "Graph.h"
+//#include "graphviewer.h"
 class TravelAgency
 {
 	Graph<Node> travelAgencyGraph;
