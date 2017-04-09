217d05b9c6454153f102a83e5ed934f46227198d
Gil Dinis Magalhães Teixeira <up201505735@ms.uporto.pt>
2017-04-09 22:52:10 +0100

    Some more documentation Done.

--- agencia de viagens/agencia de viagens/Node.h
+++ agencia de viagens/agencia de viagens/Node.h
@@ -1,3 +1,9 @@
+/**
+* @file Node.h
+* @title Node Class
+* @brief Class that stores Node Information 
+*/
+
 #ifndef NODE_H_
 #define NODE_H_
 
@@ -13,13 +19,56 @@ class Node
 	float longitude;
 	float latitude;
 public:
+	/**
+	*@brief Creates a Node
+	*@param nodeID
+	*@param cityName
+	*@param accommodation
+	*@param longitude
+	*@param latitude
+	*/
 	Node(int nodeID, string cityName, Accommodation accommodation, float longitude, float latitude);
+
+	/**
+	*@brief Gets the Node's ID.
+	*@return nodeID.
+	*/
 	int getNodeID() const;
+
+	/**
+	*@brief Gets the City's name.
+	*@return City's name.
+	*/
 	string getCityName() const;
+
+	/**
+	*@brief Gets the Accommodation.
+	*@return The accommodation.
+	*/
 	Accommodation getAccommodation() const;
+
+	/**
+	*@brief Compares two dates
+	*@param date The date to be compared
+	*/
 	bool operator==(const Node & node) const;
+
+	/**
+	*@brief Compares two dates
+	*@param date The date to be compared
+	*/
 	bool operator!=(const Node & node) const;
+
+	/**
+	*@brief Gets the Longitude of the City.
+	*@return Longitude of the City.
+	*/
 	float getLongitude() const;
+
+	/**
+	*@brief  Gets the Latitude of the City.
+	*@return Latitude of the City.
+	*/
 	float getLatitude() const;
 };
 
--- agencia de viagens/agencia de viagens/TripInfo.h
+++ agencia de viagens/agencia de viagens/TripInfo.h
@@ -1,3 +1,10 @@
+/**
+* @file TripInfo.h
+* @title Class TripInfo
+* @brief  Class that stores Information about the Trip
+*/
+
+
 #ifndef TRIP_INFO_
 #define TRIP_INFO_
 
@@ -14,10 +21,37 @@ class TripInfo
 	unsigned int time_in_minutes;
 
 public:
+	/**
+	*@brief Creates a TripInfo object.
+	*@param type Type of Transportation.
+	*@param time_in_minutes Time in minutes of the Trip.
+	*@param price_in_euros Price in euros of the Trip.
+	*/
 	TripInfo(string type, int time_in_minutes, double price_in_euros);
+
+	/**
+	*@brief Gets the Trip's type of Transportation.
+	*@return type of transportation.
+	*/
 	string getType() const;
+
+	/**
+	*@brief Gets the Trip duration.
+	*@return Time in minutes of the Trip.
+	*/
 	unsigned int getTime() const;
+
+	/**
+	*@brief Gets the Trip cost.
+	*@return Price in euros of the Trip.
+	*/
 	double getPrice() const;
+
+	/**
+	*@brief Operator to print Information about the trip.
+	*@param out	Where to print the Trip Information.
+	*@param trip The Trip to print.
+	*/
 	friend ostream& operator<<(ostream &out, const TripInfo &trip);
 };
 
